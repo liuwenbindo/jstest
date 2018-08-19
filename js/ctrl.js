bd257faf3a2cf2741e7e59b3c729ad5f0b9c3322
liuwenbindo <liuwenbindo@outlook.com>
2018-08-18 23:20:49 -0400

    Init Commit

---
+++ js/ctrl.js
@@ -0,0 +1,69 @@
+function generate_table() {
+  if (document.getElementById("corr table") == null) {
+    var body = document.getElementsByTagName("body")[0];
+    var table = document.createElement("table");
+    var tbl_body = document.createElement("tbody");
+
+    table.appendChild(tbl_body);
+    table.setAttribute("id", "corr table");
+    body.appendChild(table);
+
+    // Add function to read the correlation Matrix
+
+    // Ajax solution
+    // $(document).ready(function() {
+    //     $.ajax({
+    //         type: "GET",
+    //         url: "./sample.csv",
+    //         dataType: "text",
+    //         success: function(data) { console.log(data); }
+    //      });
+    // });
+    var txt = '';
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+      if(xmlhttp.status == 200 && xmlhttp.readyState == 4){
+        txt = xmlhttp.responseText;
+      }
+    };
+    xmlhttp.open("GET","sample.csv",true);
+    xmlhttp.send();
+
+    // Read csv as string
+
+    // Split the csv string to get csv content
+
+    import_corr(ticker_list);
+  }
+  else {
+    console.log("Already created.")
+  }
+}
+
+ticker_list = ['AAPL US Equity', 'AMZN US Equity','VIX Index'];
+
+function import_corr(ticker_list){
+  var tbl_body = document.getElementById("corr table").getElementsByTagName("tbody")[0];
+  for (var i = 0; i < ticker_list.length + 1; i++){
+    var row = document.createElement("tr"); //this row
+    for (var j = 0; j < ticker_list.length + 1; j++){
+      var cell = document.createElement("td");
+      if (i == 0 & j == 0) { cell.innerHTML = ""; }
+      else if (i == 0) { cell.innerHTML = ticker_list[j-1]; }
+      else if (j == 0) { cell.innerHTML = ticker_list[i-1]; }
+      else { cell.innerHTML = 0.1 }
+      row.appendChild(cell);
+    }
+    tbl_body.appendChild(row);
+  }
+}
+
+
+
+function split_data(data_text){
+  var all_lines = data_text.split(/\r\n|\n/);
+  var lines = []
+  for (var i = 0; i < all_lines.length; i++) {
+    lines.push(all_lines[i]);
+  }
+}
